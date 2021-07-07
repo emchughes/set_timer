14ce786148e837fc71a614bc7333fc92c0de3375
Elizabeth McHughes <eliza.mchughes@gmail.com>
2021-07-07 14:44:28 -0500

    timer update needs more work

--- src/App.js
+++ src/App.js
@@ -5,15 +5,10 @@ import React from "react";
 import TimeField from "react-simple-timefield";
 
 function convertTime(time) {
-  console.log(time);
   const hour = time.substring(0, 2);
-  console.log(hour);
   const minute = time.substring(3, 5);
-  console.log(minute);
   const second = time.substring(6, 8);
-  console.log(second);
   let ms = hour * 3600000 + minute * 60000 + second * 1000;
-  console.log(ms);
   return ms;
 }
 
@@ -21,31 +16,35 @@ class CountDownTimer extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      time: "00:00:00"
-    };
-    this.onTimeChange = this.onTimeChange.bind(this);
+      time: "12:00:00"
+    }
+    this.onTimeChange = this.onTimeChange.bind(this)
   }
   onTimeChange(event, time) {
-    this.setState({ time });
-  }
-
+    console.log(time)
+    this.setState({ time })
+    console.log(time)
+    }
+    
   render() {
-    const { time } = this.state;
+    const { time } = this.state
     return (
       <div>
         <div>
           <TimeField value={time} onChange={this.onTimeChange} showSeconds />
         </div>
+        <p>{time}</p>
         <h3>
           <Timer
-            initialTime={convertTime(this.state.time)}
+            initialTime={convertTime(time)}
             direction="backward"
             startImmediately={false}
           >
-            {({ start, resume, pause, stop, reset, timerState }) => (
+            {({ start, resume, pause, stop, reset, set }) => (
               <React.Fragment>
                 <div>
                   <Timer.Hours /> h <Timer.Minutes /> m <Timer.Seconds /> s
+                    <button onClick{() => console}>Enter</button>
                 </div>
                 <br />
                 <div>
