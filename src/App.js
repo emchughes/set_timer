489841fb90a40016693399d147359a2f31e27507
Elizabeth McHughes <eliza.mchughes@gmail.com>
2021-07-08 15:10:13 -0500

    going to try making a new timer

--- src/App.js
+++ src/App.js
@@ -4,61 +4,66 @@ import Timer from "react-compound-timer";
 import React from "react";
 import TimeField from "react-simple-timefield";
 
-let setTime = "00:00:00"
+function convertTime(time) {
+  console.log("convertTime time: " + time);
+  const hour = time.substring(0, 2);
+  const minute = time.substring(3, 5);
+  const second = time.substring(6, 8);
+  let ms = hour * 3600000 + minute * 60000 + second * 1000;
+  console.log(ms);
+  return ms;
+}
 
 class CountDownTimer extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      time: "12:00:00",
-    }
-    this.onTimeChange = this.onTimeChange.bind(this)
+      time: "",
+      initTime: 0
+    };
+    this.onTimeChange = this.onTimeChange.bind(this);
+    this.doIt = this.doIt.bind(this);
   }
   onTimeChange(event, time) {
-    console.log("setTime before: " + setTime)
-    console.log("time before: " + time)
-    this.setState({ time, setTime})
-    setTime = time
-    console.log("setTime after: " + setTime)
+    this.setState({ time });
   }
-  convertTime(setTime) {
-    console.log("convertTime: " + setTime)
-    const hour = this.state.time.substring(0, 2);
-    const minute = this.state.time.substring(3, 5);
-    const second = this.state.time.substring(6, 8);
-    let ms = hour * 3600000 + minute * 60000 + second * 1000;
-    return ms;
+  doIt(evt) {
+    this.setState({ initTime: convertTime(this.state.time) });
   }
 
   render() {
-    const { time } = this.state
-    const { setTime } = this.state
+    const { time, initTime } = this.state;
+    console.log(time);
+    console.log(initTime);
     return (
       <div>
         <h3>
           <TimeField value={time} onChange={this.onTimeChange} showSeconds />
-          <Timer
-            initialTime={this.convertTime(setTime)}
-            direction="backward"
-            startImmediately={false}
+          <button onClick={this.doIt}>Submit</button>
+          <p>{this.state.time}</p>
+          {!!initTime && (
+            <Timer
+              initialTime={initTime}
+              direction="backward"
+              startImmediately={false}
             >
-            
-            {({ start, resume, pause, stop, reset }) => (
-              <React.Fragment>
-                <div>
-                  <Timer.Hours /> h <Timer.Minutes /> m <Timer.Seconds /> s
-                </div>
-                <br />
-                <div>
-                  <button onClick={start}>Start</button>
-                  <button onClick={pause}>Pause</button>
-                  <button onClick={resume}>Resume</button>
-                  <button onClick={stop}>Stop</button>
-                  <button onClick={reset}>Reset</button>
-                </div>
-              </React.Fragment>
-            )}
-          </Timer>
+              {({ start, resume, pause, stop, reset, setTime }) => (
+                <React.Fragment>
+                  <div>
+                    <Timer.Hours /> h <Timer.Minutes /> m <Timer.Seconds /> s
+                  </div>
+                  <br />
+                  <div>
+                    <button onClick={start}>Start</button>
+                    <button onClick={pause}>Pause</button>
+                    <button onClick={resume}>Resume</button>
+                    <button onClick={stop}>Stop</button>
+                    <button onClick={reset}>Reset</button>
+                  </div>
+                </React.Fragment>
+              )}
+            </Timer>
+          )}
         </h3>
       </div>
     );
